894c78f2e644c8412586786a1fa91cff588b8038
Jess Robinson <castaway@desert-island.me.uk>
2019-06-05 14:59:06 +0000

    Syntax typos

--- scrapers/lib/Event/Scraper/Website/FriendsOfTwigs.pm
+++ scrapers/lib/Event/Scraper/Website/FriendsOfTwigs.pm
@@ -27,7 +27,7 @@ sub get_events {
         );
     my @events;
     my $now  = DateTime->now();
-    my %months => ( 'January' => 1, February => 2, March => 3, April => 4, May => 5, June => 6, July => 7, August => 8, September => 9, October => 10, November => 11, December => 12);
+    my %months = ( 'January' => 1, February => 2, March => 3, April => 4, May => 5, June => 6, July => 7, August => 8, September => 9, October => 10, November => 11, December => 12);
     foreach my $div (@event_divs) {
         my $name = $div->look_down('class' => 'module-title');
         next if !$name;
